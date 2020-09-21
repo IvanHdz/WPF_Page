35ff07fcf42b803f66c0e5a4c843c54cde39d256
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-09-21 12:48:17 -0500

    Commit

---
+++ Page.WPF/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Page.WPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainPage.xaml
+    /// </summary>
+    public partial class MainPage : System.Windows.Controls.Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
